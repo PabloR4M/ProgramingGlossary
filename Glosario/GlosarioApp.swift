434b689a0bb43cd5d40eafb57943b57778886fe0
PabloR4M <ramospablofrancisco@gmail.com>
2026-05-17 14:30:25 -0700

    Initial Commit

---
+++ Glosario/GlosarioApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlosarioApp.swift
+//  Glosario
+//
+//  Created by Pablo on 17/05/26.
+//
+
+import SwiftUI
+
+@main
+struct GlosarioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
